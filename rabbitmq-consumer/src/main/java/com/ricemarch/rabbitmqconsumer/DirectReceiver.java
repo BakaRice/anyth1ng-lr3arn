fd02c0610ca7b6271bd08768ec8cf78b1f83e8e7
ricemarch <ricemarch@foxmail.com>
2021-09-24 18:12:33 +0800

    init rabbitmq-consumer

---
+++ rabbitmq-consumer/src/main/java/com/ricemarch/rabbitmqconsumer/DirectReceiver.java
@@ -0,0 +1,35 @@
+package com.ricemarch.rabbitmqconsumer;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+/**
+ * @author tanwentao
+ * @since 2021/9/24
+ */
+
+@Component
+//监听的队列名称 TestDirectQueue
+@Lazy(value = false)
+@Slf4j
+public class DirectReceiver {
+
+    @RabbitListener(queues = "TestDirectQueue")
+    public void process0(Map<?,?> testMessage) {
+        log.info("[0]DirectReceiver消费者收到消息:" + testMessage.toString());
+    }
+
+    @RabbitListener(queues = "TestDirectQueue")
+    public void process1(Map<?,?> testMessage) {
+        log.info("[1]DirectReceiver消费者收到消息:" + testMessage.toString());
+    }
+
+    @RabbitListener(queues = "TestDirectQueue")
+    public void process2(Map<?,?> testMessage) {
+        log.info("[2]DirectReceiver消费者收到消息:" + testMessage.toString());
+    }
+}
