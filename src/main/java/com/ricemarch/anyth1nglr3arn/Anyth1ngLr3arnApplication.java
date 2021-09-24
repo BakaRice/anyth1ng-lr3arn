a5b24681903504a8a53c180b37421e584e1cfec7
ricemarch <ricemarch@foxmail.com>
2021-09-24 14:23:27 +0800

    init

---
+++ src/main/java/com/ricemarch/anyth1nglr3arn/Anyth1ngLr3arnApplication.java
@@ -0,0 +1,13 @@
+package com.ricemarch.anyth1nglr3arn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Anyth1ngLr3arnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Anyth1ngLr3arnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ricemarch/anyth1nglr3arn/Anyth1ngLr3arnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricemarch.anyth1nglr3arn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Anyth1ngLr3arnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
