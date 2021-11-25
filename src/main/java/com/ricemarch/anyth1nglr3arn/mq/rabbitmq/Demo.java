e93808ca502a2c0aa1a8e1b37621ca904a5e40f8
ricemarch <ricemarch@foxmail.com>
2021-11-25 16:59:28 +0800

    Integer test

--- src/main/java/com/ricemarch/anyth1nglr3arn/mq/rabbitmq/Demo.java
+++ src/main/java/com/ricemarch/anyth1nglr3arn/mq/rabbitmq/Demo.java
@@ -6,4 +6,9 @@ package com.ricemarch.anyth1nglr3arn.mq.rabbitmq;
  */
 
 public class Demo {
+    public static void main(String[] args) {
+        Integer t = null;
+        Integer t1 = 1;
+        System.out.println(t1.equals(t));
+    }
 }
